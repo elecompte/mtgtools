63f6ca946652205bd772d732de1daf02b5b8f38e
Ed LeCompte <ed@lecompte.us>
2025-06-09 14:21:06 -0400

    Create menu page

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route('/')
 def index():
-    final_swarms = 0
+    return render_template('index.html')
+
+@app.route('/scuteswarm', methods=['GET', 'POST'])
+def scuteswarm():
+    final_swarms = None
     initial = 0
     lands = 0
-    if request.method == "POST":
+    if request.method == 'POST':
         try:
-            initial = int(request.form["initial"])
-            lands = int(request.form["lands"])
+            initial = int(request.form['initial'])
+            lands = int(request.form['lands'])
             final_swarms = initial * (2 ** lands)
-        except ValueError:
-            final_swarms = "Invalid input. Please enter integers only!"
-
-    return render_template("scuteswarm.html", final_swarms=final_swarms, initial=initial, lands=lands)
+        except Exception:
+            final_swarms = None
+    return render_template('scuteswarm.html', final_swarms=final_swarms, initial=initial, lands=lands)
 
 if __name__ == "__main__":
     app.run(debug=True)
