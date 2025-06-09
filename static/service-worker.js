b9fc815bf97f46e25d7eb3b80d4879de9e862bcc
Ed LeCompte <ed@lecompte.us>
2025-06-09 12:10:16 -0400

    Add PWA

---
+++ static/service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('scuteswarm-cache-v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/static/lands.jpg',
+        '/static/scuteswarm.jpg',
+        '/static/scuteswarm.png',
+        '/static/manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
